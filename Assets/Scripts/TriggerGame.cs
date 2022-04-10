9566892c164e73e0d3f1edbe5abfeb46392b511a
Charles Lyster <saltystudiosinc@gmail.com>
2022-04-10 15:34:36 +0200

    Proto-Lobby
    Prototyped the lobby and added 3 other gamegates. Created script that will activate the respective scene.

---
+++ Assets/Scripts/TriggerGame.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerGame : MonoBehaviour
+{
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("start game!");
+        
+        //get portal tag
+        //load respective scene through Scenemanager
+    }
+}
