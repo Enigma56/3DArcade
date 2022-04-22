ad14014569c5a169b227ec370a804d1ea39f7793
Charles Lyster <saltystudiosinc@gmail.com>
2022-04-22 15:10:35 +0200

    Adds TMP & Lobby detail
    Adds text to each portal so the player knows what they are entering. Makes each portal a complete prefab by replacing old portals. Changed up texturing of player and floor

--- Assets/Scripts/TriggerGame.cs
+++ Assets/Scripts/TriggerGame.cs
@@ -1,14 +1,39 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TriggerGame : MonoBehaviour
 {
     
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("start game!");
+        
+        /*Debug.Log(other.gameObject.tag);
+        if (other.gameObject.CompareTag("BlockStack"))
+        {
+            Debug.Log("spaghetti!");
+        }*/
+
+        //Only player is colliding, no need to check "other"
+        var gameTag = gameObject.tag;
+        switch (gameTag)
+        {
+            case "BlockStack":
+                Debug.Log("blockstack loaded!");
+                SceneManager.LoadScene("BlockStack");
+                break;
+            case "SkiBall":
+                Debug.Log("skiball loaded!");
+                break;
+            case "SpaceShooter":
+                Debug.Log("galaga loaded!");
+                break;
+            
+        }
+        //Debug.Log("start game!");
         
         //get portal tag
         //load respective scene through Scenemanager
